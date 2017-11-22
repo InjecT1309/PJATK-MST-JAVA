57f2a29e213372292427a019fb7eb40d545d632d
Adam <adamail907@gmail.com>
2017-11-23 00:48:43 +0100

    added file handling, node moving, node and connection displaying and some buttons

---
+++ src/pjatk/s16787/mst/NodeMover.java
@@ -0,0 +1,60 @@
+package pjatk.s16787.mst;
+
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+
+public class NodeMover implements MouseListener
+{
+	Node[] node;
+	
+	public NodeMover(Node[] node)
+	{
+		this.node = node;
+	}
+	
+	public void mousePressed(MouseEvent e) 
+	{
+		int pt_x, pt_y;
+		
+		for(Node tmp : node)
+		{
+			pt_x = e.getX();
+			pt_y = e.getY();
+			
+			if(tmp.checkIfPointInNode(pt_x, pt_y))
+			{
+				tmp.pickUp();
+			}
+		}
+	}
+	public void mouseReleased(MouseEvent e) 
+	{
+		int pt_x, pt_y;
+		
+		for(Node tmp : node)
+		{
+			if(tmp.is_movable == true)
+			{
+				pt_x = e.getX();
+				pt_y = e.getY();
+				
+				tmp.setCenterPosition(pt_x, pt_y);
+			}
+		}
+	}
+
+	public void mouseEntered(MouseEvent e) 
+	{
+	
+	}
+
+	public void mouseExited(MouseEvent e) 
+	{
+	
+	}
+
+	public void mouseClicked(MouseEvent e) 
+	{
+		
+	}
+}
